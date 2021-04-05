c957e1e40d3e7463450ba6debd490087041557ab
poxx1 <julianlastrama@outlook.com>
2021-04-05 10:09:33 -0300

    Hello world

---
+++ happy.ino
@@ -0,0 +1,75 @@
+#include "pitches.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  //SOL SOL LA SOL DO SI
+  tone(8, NOTE_G5,1000/4);
+    delay(250);
+  tone(8, NOTE_G5,1000/4);
+    delay(250);
+  tone(8, NOTE_A5,1000/1);
+    delay(500);
+  tone(8, NOTE_G5,1000/2);
+    delay(1000);
+  tone(8, NOTE_C5,1000/1);
+    delay(500);
+  tone(8, NOTE_B5,1000/2);
+    delay(1000);
+    
+      //SOL SOL LA SOL RE DO
+  tone(8, NOTE_G5,1000/4);
+    delay(250);
+  tone(8, NOTE_G5,1000/4);
+    delay(250);
+  tone(8, NOTE_A5,1000/1);
+    delay(500);
+  tone(8, NOTE_G5,1000/2);
+    delay(1000);
+  tone(8, NOTE_D5,1000/1);
+    delay(500);
+  tone(8, NOTE_C5,1000/2);
+    delay(1000);
+
+    //SOL SOL SOL MI 
+  tone(8, NOTE_G5,1000/4);
+    delay(250);
+  tone(8, NOTE_G5,1000/4);
+    delay(250);
+  tone(8, NOTE_G6,1000/1);
+    delay(500);
+  tone(8, NOTE_E5,1000/2);
+    delay(1000);
+
+    //DO DO SI LA
+      tone(8, NOTE_C6,1000/4);
+    delay(250);
+  tone(8, NOTE_C6,1000/4);
+    delay(250);
+  tone(8, NOTE_B6,1000/1);
+    delay(500);
+  tone(8, NOTE_A5,1000/2);
+    delay(1000);
+
+    //FA FA MI DO
+    tone(8, NOTE_F6,1000/4);
+    delay(250);
+  tone(8, NOTE_F6,1000/4);
+    delay(250);
+  tone(8, NOTE_E6,1000/1);
+    delay(500);
+  tone(8, NOTE_C5,1000/2);
+    delay(1000);
+
+    //RE DO
+  tone(8, NOTE_D5,1000/2);
+    delay(500);
+  tone(8, NOTE_C5,1000/4);
+    delay(1000);
+
+    setup();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
